b6e1321e403c257d4fe2f72179efe7b003279bfa
BlackB34rd <contact@the-tiny-spark.com>
2026-06-08 12:06:10 +0200

    add tp n°2 correctioné

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,41 @@
-
-type IdCard = {
-    name:string, 
-    age:number, 
-    job?: string 
-    // le point d'interrogation permet de rendre une propriété optionnelle
+type Employee = {
+  id: number;
+  name: string;
 };
 
-const profiles:IdCard[] = []; 
+type Salary = {
+  employeeId: number;
+  amount: number;
+};
 
-profiles.push(
-    {
-        age: 40, 
-        // job: "developer", 
-        name: "Nicolas"
+type EmployeeWithSalary = {
+  id: number;
+  name: string;
+  salary: number;
+};
+
+
+const employees:Map<number, Employee> = new Map<number, Employee>();
+employees.set(1,{id:1, name:"Nicolas"});
+employees.set(2,{id:2, name:"Matthieu"});
+employees.set(3,{id:3, name:"Gabriel"});
+employees.set(4,{id:4, name:"Cyril"});
+
+const salaries:Map<number, Salary> = new Map<number, Salary>();
+salaries.set(1,{employeeId:1, amount:4000});
+salaries.set(2,{employeeId:2, amount:6500});
+salaries.set(3,{employeeId:3, amount:10000});
+salaries.set(4,{employeeId:4, amount:10000});
+
+
+const results = Array.from(employees.values()).map( 
+    (employee:Employee):EmployeeWithSalary=>{
+        return {
+            id: employee.id, 
+            name: employee.name, 
+            salary : salaries.get(employee.id)?.amount || 0
+        }
     }
 );
 
-
-const myMap = new Map<string, number>();
-myMap.set("Cyril",44);
-myMap.set("Gabriel",37);
-myMap.set("Matthieu",39);
-myMap.set("Nicolas",32);
-
-
-myMap.delete("Nicolas");
-myMap.clear();
-
-
-
-console.log(myMap);
+console.log(results);
