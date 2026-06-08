4ce66e5e721aebc450303d1c241bb6c4d25db6c3
BlackB34rd <contact@the-tiny-spark.com>
2026-06-08 10:41:05 +0200

    add enum example

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,11 @@
+enum PointCardinal{
+    NORTH = "PointCardinal_NORTH", 
+    SOUTH = "SOUTH", 
+    EAST = "EAST", 
+    WEST = "WEST"
+}; 
 
 
-// mon objet ici, va occuper une certaine adresse en mémoire
-// MAIS on est en javascript, donc cet objet est mouvant par nature
-// TANT QUE l'adresse en mémoire qui donne accès à l'objet ne 
-// change pas, c'est ok pour Javascript, même si le contenu 
-// de l'objet en lui-même change 
-const profil:any = {
-    name: "Nicolas", 
-    age: 40
-};
+const direction: PointCardinal = PointCardinal.NORTH; 
 
-
-profil.name = "Cyril";
-profil.age = 44;
-
-// console.log(profil);
-
-
-function displayMyAge(show:boolean){
-    
-    if( show === true ){
-        // le mot clé let se limite à une portée locale
-        var age:number = 40; // var permet une portée globale
-    }
-    
-    console.log(age);
-}
-
-displayMyAge(true);
-
-
-
-
-
+console.log(direction);
