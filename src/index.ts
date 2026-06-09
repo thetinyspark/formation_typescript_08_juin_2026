0785f49711a99b98b94dbaf3e8c66f86659f5e80
BlackB34rd <contact@the-tiny-spark.com>
2026-06-09 13:53:12 +0200

    add tp n°11 correction

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,26 @@ import AccorArena from "./arena/AccorArena";
 import Colosseum from "./arena/Colosseum";
 import { IArena } from "./arena/IArena";
 import DynamicFactory from "./factory/DynamicFactory";
+import MagicStaff from "./weapons/MagicStaff";
+import Sword from "./weapons/Sword";
+import Weapon from "./weapons/Weapon";
 
 const factory = new DynamicFactory();
-const production = true;
-const ARENA_BUILDER_KEY:string = "ARENA";
+const production = false;
+const ARENA_BUILDER_KEY:string = "ARENA_BUILDER_KEY";
+const WEAPON_BUILDER_KEY = "WEAPON_BUILDER_KEY"; 
 
 if( production){
     factory.addBuilder(ARENA_BUILDER_KEY, ()=>new AccorArena()); 
+    factory.addBuilder(WEAPON_BUILDER_KEY, ()=>new Sword()); 
 }
 else{   
     factory.addBuilder(ARENA_BUILDER_KEY, ()=>new Colosseum()); 
+    factory.addBuilder(WEAPON_BUILDER_KEY, ()=>new MagicStaff()); 
 }
 
 
+const json = factory.resolve<Weapon>(WEAPON_BUILDER_KEY)?.toJSON();
 factory.resolve<IArena>(ARENA_BUILDER_KEY)?.startFight();
+
+console.log(json);
--- src/weapons/Weapon.ts
+++ src/weapons/Weapon.ts
@@ -1,4 +1,15 @@
 export default class Weapon{
     public strength:number = 0; 
     public durability:number = 0; 
+
+    public toJSON():string{
+
+        let output:string = "{";
+        for( var prop in this){
+            output += `\r\n"${prop}": ${this[prop]},`+"\r\n";
+        }
+
+        output += "}"; 
+        return output;
+    }
 }
