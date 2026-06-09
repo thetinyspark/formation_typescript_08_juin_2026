e8e0ce544f51f95a6c4e16686bead0085f77e2af
BlackB34rd <contact@the-tiny-spark.com>
2026-06-09 14:04:58 +0200

    add singleton management on dynamic factory

--- src/factory/DynamicFactory.ts
+++ src/factory/DynamicFactory.ts
@@ -1,19 +1,56 @@
 // le concept du pattern factory est de créer
 // des instances d'autres objets. 
+type BuilderInfo = { singleton:boolean, builder:Function, uniqInstance:any}; 
 
 export default class DynamicFactory{
-    private _builders:Map<string, Function> = new Map<string, Function>();
+    private _builders:Map<string, BuilderInfo> = new Map<string, BuilderInfo>();
 
-    public addBuilder(key:string, func:Function):void{
-        this._builders.set(key, func);
+    public addBuilder(key:string, func:Function, singleton:boolean = false):void{
+        this._builders.set(
+            key, 
+            {
+                singleton: singleton,
+                builder:func, 
+                uniqInstance: null
+            }
+        );
     }
 
     public resolve<T>(key:string):T|null{
+
+        let instance:T|null = null;
+
+        // si on a un builder qui répond à la clé
         if( this._builders.has(key)){
-            const func = this._builders.get(key); 
-            return func?.apply(null) as T;
+
+            // alors on va chercher le builderinfo en question
+            const builderInfo:BuilderInfo|null = this._builders.get(key) || null; 
+
+            // si le builderInfo doit gérer une instance unique
+            if( builderInfo?.singleton === true){
+
+                // que cette instance unique existe déjà
+                if( builderInfo?.uniqInstance !== null){
+
+                    // alors on retourne l'instance unique
+                    instance = builderInfo?.uniqInstance as T;
+                }
+                else{
+
+                    // sinon on crée une nouvelle instance que l'on stocke
+                    // sur builderinfo et que l'on retourne à la fin
+                    instance = builderInfo?.builder?.apply(null) as T;
+                    builderInfo.uniqInstance = instance;
+                }
+            }
+            else{
+                // si le builderinfo ne doit pas gérer d'instance unique, 
+                // alors on retourne une nouvelle instance à chaque fois
+                instance = builderInfo?.builder?.apply(null) as T;
+            }
+
         }
 
-        return null;
+        return instance;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,13 @@
 import AccorArena from "./arena/AccorArena";
-import Colosseum from "./arena/Colosseum";
 import { IArena } from "./arena/IArena";
 import DynamicFactory from "./factory/DynamicFactory";
-import MagicStaff from "./weapons/MagicStaff";
-import Sword from "./weapons/Sword";
-import Weapon from "./weapons/Weapon";
 
 const factory = new DynamicFactory();
-const production = false;
 const ARENA_BUILDER_KEY:string = "ARENA_BUILDER_KEY";
-const WEAPON_BUILDER_KEY = "WEAPON_BUILDER_KEY"; 
 
-if( production){
-    factory.addBuilder(ARENA_BUILDER_KEY, ()=>new AccorArena()); 
-    factory.addBuilder(WEAPON_BUILDER_KEY, ()=>new Sword()); 
-}
-else{   
-    factory.addBuilder(ARENA_BUILDER_KEY, ()=>new Colosseum()); 
-    factory.addBuilder(WEAPON_BUILDER_KEY, ()=>new MagicStaff()); 
-}
+factory.addBuilder(ARENA_BUILDER_KEY, ()=>new AccorArena(), true); 
 
+const instance1 = factory.resolve<IArena>(ARENA_BUILDER_KEY);
+const instance2 = factory.resolve<IArena>(ARENA_BUILDER_KEY);
 
-const json = factory.resolve<Weapon>(WEAPON_BUILDER_KEY)?.toJSON();
-factory.resolve<IArena>(ARENA_BUILDER_KEY)?.startFight();
-
-console.log(json);
+console.log(instance1 === instance2);
